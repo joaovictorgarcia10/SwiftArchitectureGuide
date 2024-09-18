2b0ac2ce4bdffc5521d89808248e25510576cadc
João Victor Garcia Leite Santana <jvictor_sk8@hotmail.com>
2024-09-17 21:12:18 -0300

    Initial Commit

---
+++ SwiftArchitectureGuide/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftArchitectureGuide
+//
+//  Created by Joao Victor Garcia Leite Santana on 17/09/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
